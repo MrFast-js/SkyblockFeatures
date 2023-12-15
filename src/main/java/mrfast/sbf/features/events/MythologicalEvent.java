fca89b1b287bb14885f7753dd9f1aa24c10f50dc
MrFast-js <mrfastkrunker@gmail.com>
2023-12-15 14:22:36 -0500

    Mythological Event revamp, better waypoints

--- src/main/java/mrfast/sbf/features/events/MythologicalEvent.java
+++ src/main/java/mrfast/sbf/features/events/MythologicalEvent.java
@@ -2,7 +2,10 @@ package mrfast.sbf.features.events;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
 
@@ -18,10 +21,7 @@ import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.projectile.EntityFishHook;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.server.S2APacketParticles;
-import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.BlockPos;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.Vec3;
+import net.minecraft.util.*;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.world.WorldEvent;
@@ -29,7 +29,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 public class MythologicalEvent {
-    private BlockPos burrow = null;
+    private List<Burrow> burrows = new ArrayList<>();
+
+    public static class Burrow {
+        String type;
+        BlockPos pos;
+
+        public Burrow(BlockPos pos) {
+            this.pos = pos;
+
+            this.type = "Particle";
+        }
+    }
+
     private BlockPos prevBurrow = null;
     private boolean sendNotification = true;
     private static final List<Vec3> particles = new ArrayList<>();
@@ -62,14 +74,22 @@ public class MythologicalEvent {
     }
 
     private void handleBurrowParticles(S2APacketParticles packet) {
-        if (burrow == null || (burrow.getX() != Math.floor(packet.getXCoordinate()) && burrow.getZ() != Math.floor(packet.getZCoordinate()))) {
+        boolean dupe = false;
+        for (Burrow burrow : burrows) {
+            if (burrow.pos.getX() == Math.floor(packet.getXCoordinate()) || burrow.pos.getZ() == Math.floor(packet.getZCoordinate())) {
+                dupe = true;
+            }
+        }
+        if (!dupe) {
             if (sendNotification) {
                 sendNotification = false;
                 Utils.sendMessage(ChatFormatting.GREEN + "Located a new Griffin Burrow!");
                 Utils.playSound("random.orb", 0.1);
                 Utils.setTimeout(() -> sendNotification = true, 15 * 1000);
             }
-            burrow = new BlockPos(Math.floor(packet.getXCoordinate()), Math.floor(packet.getYCoordinate()), Math.floor(packet.getZCoordinate()));
+            BlockPos pos = new BlockPos(Math.floor(packet.getXCoordinate()), Math.floor(packet.getYCoordinate() - 1), Math.floor(packet.getZCoordinate()));
+            Burrow burrow = new Burrow(pos);
+            burrows.add(burrow);
         }
     }
 
@@ -106,16 +126,46 @@ public class MythologicalEvent {
     private void handleAncestralParticles(S2APacketParticles packet) {
         if (Utils.GetMC().thePlayer.getHeldItem() != null && Utils.GetMC().thePlayer.getHeldItem().getDisplayName().contains("Ancestral") && SkyblockFeatures.config.MythologicalHelper) {
             if (packet.getParticleType() == EnumParticleTypes.DRIP_LAVA) {
+                Vec3i pos = new Vec3i(packet.getXCoordinate(), packet.getYCoordinate(), packet.getZCoordinate());
                 double dist = Utils.GetMC().thePlayer.getDistance(packet.getXCoordinate(), packet.getYCoordinate(), packet.getZCoordinate());
                 if (dist > 3 && dist < 5) {
                     particles.clear();
                 }
+                Burrow closest = getClosestBurrow(pos);
 
-                if (dist > 2 && !particles.contains(new Vec3(packet.getXCoordinate(), packet.getYCoordinate(), packet.getZCoordinate()))) {
+                if (dist > 2 && !particles.contains(new Vec3(pos))) {
+                    if (closest != null) {
+                        if (Utils.GetMC().thePlayer.getDistanceSq(closest.pos) < 50) {
+                            return;
+                        }
+                    }
                     particles.add(new Vec3(packet.getXCoordinate(), packet.getYCoordinate(), packet.getZCoordinate()));
+                }
+
+                if (closest != null) {
+                    closest.type = "Treasure";
+                }
+            }
+            if (packet.getParticleType() == EnumParticleTypes.CRIT) {
+                Vec3i pos = new Vec3i(packet.getXCoordinate(), packet.getYCoordinate(), packet.getZCoordinate());
+
+                Burrow closest = getClosestBurrow(pos);
+
+                if (closest != null && closest.pos.distanceSq(pos) < 3) {
+                    closest.type = "Mob";
                 }
             }
         }
+    }
+
+    public Burrow getClosestBurrow(Vec3i pos) {
+        Burrow closest = null;
+        for (Burrow burrow : burrows) {
+            if (closest == null || burrow.pos.distanceSq(closest.pos) < closest.pos.distanceSq(pos)) {
+                closest = burrow;
+            }
+        }
+        return closest;
     }
 
     @SubscribeEvent
@@ -125,16 +175,35 @@ public class MythologicalEvent {
         }
 
         String unformatted = Utils.cleanColor(event.message.getUnformattedText());
-        if (unformatted.contains("You dug out a Griffin Burrow") || unformatted.contains("You finished the Griffin burrow chain")) {
-            endPos = null;
-            startPos = null;
-            particles.clear();
-            Utils.setTimeout(() -> {
-                prevBurrow = burrow;
-                burrow = null;
-                sendNotification = true;
-            }, 300);
+        if (unformatted.startsWith("You dug out a Griffin Burrow") || unformatted.startsWith("You finished the Griffin burrow chain")) {
+            reset(true);
         }
+        if (unformatted.startsWith("You dug out a Griffin Burrow") || unformatted.startsWith("You finished the Griffin burrow chain") || unformatted.startsWith("Wow! You dug out") || unformatted.startsWith("Uh oh! You dug out") || unformatted.startsWith("Oh! You dug out") || unformatted.startsWith("Oi! You dug out") || unformatted.startsWith("Danger! You dug out") || unformatted.startsWith("Woah! You dug out") || unformatted.startsWith("Good Grief! You dug out")) {
+            reset(false);
+        }
+    }
+
+    public void reset(boolean silent) {
+        endPos = null;
+        startPos = null;
+        particles.clear();
+
+        Utils.setTimeout(() -> {
+            Comparator<Burrow> burrowComparator = Comparator.comparingDouble(burrow -> Utils.GetMC().thePlayer.getDistanceSq(burrow.pos));
+
+            List<Burrow> sortedBurrows = new ArrayList<>(burrows);
+            sortedBurrows.sort(burrowComparator);
+
+            if (!sortedBurrows.isEmpty()) {
+                Burrow closestBurrow = sortedBurrows.get(0);
+                if (closestBurrow != null) {
+                    prevBurrow = closestBurrow.pos;
+                    if (!silent) sendNotification = true;
+                }
+                sortedBurrows = sortedBurrows.stream().filter((a) -> Utils.GetMC().thePlayer.getDistanceSq(a.pos) > 25).collect(Collectors.toList());
+                burrows = sortedBurrows;
+            }
+        }, 300);
     }
 
     @SubscribeEvent
@@ -167,6 +236,7 @@ public class MythologicalEvent {
                 double yDif;
                 double index = 0;
                 for (Vec3 particle : particles) {
+
                     index++;
                     if (prev == null) {
                         prev = particle;
@@ -195,12 +265,13 @@ public class MythologicalEvent {
     private void renderBurrowBoundingBox(float partialTicks) {
         if (!Utils.inSkyblock || !SkyblockFeatures.config.MythologicalHelper) return;
 
-        if (burrow != null) {
-            AxisAlignedBB aabb2 = new AxisAlignedBB(burrow, burrow.add(1, 1, 1));
-            RenderUtil.drawOutlinedFilledBoundingBox(aabb2, Color.green, partialTicks);
+        try {
+            for (Burrow burrow : burrows) {
+                String type = ChatFormatting.RED + "(" + burrow.type + ")";
+                RenderUtil.drawWaypoint(burrow.pos, Color.green, ChatFormatting.GOLD + "Burrow " + type, partialTicks, true);
+            }
+        } catch (Exception ignored) {
 
-            AxisAlignedBB aabb = new AxisAlignedBB(burrow.getX() + 0.5, burrow.getY() + 100, burrow.getZ() + 0.5, burrow.getX() + 0.5, burrow.getY(), burrow.getZ() + 0.5);
-            RenderUtil.drawOutlinedFilledBoundingBox(aabb, Color.green, partialTicks);
         }
     }
 
@@ -244,7 +315,7 @@ public class MythologicalEvent {
 
     @SubscribeEvent
     public void onWorldLoad(WorldEvent.Load event) {
-        burrow = null;
+        burrows.clear();
         startPos = null;
         endPos = null;
         geyser = null;
