f0d954f072bcfdded6aa746a4dee006e6a03da0d
MrFast <mrfastkrunker@gmail.com>
2024-01-19 15:42:41 -0500

    Add hovering text checking into manager

--- src/main/java/mrfast/sbf/gui/SideMenu/SideMenuManager.java
+++ src/main/java/mrfast/sbf/gui/SideMenu/SideMenuManager.java
@@ -1,6 +1,10 @@
 package mrfast.sbf.gui.SideMenu;
 
+import mrfast.sbf.mixins.transformers.GuiContainerAccessor;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.input.Keyboard;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +18,33 @@ public class SideMenuManager {
             sidebarMap.put(gui, new SideMenu());
         }
         return sidebarMap.get(gui);
+    }
+
+    @SubscribeEvent
+    public void onGuiScreenDrawn(GuiScreenEvent.DrawScreenEvent.Post event) {
+        if (event.gui instanceof GuiContainer) {
+            SideMenu sidebar = SideMenuManager.getOrCreateSidebar((GuiContainer) event.gui);
+            // Draw sidebar hovering text
+            for (CustomElement element : sidebar.getElements().values()) {
+                element.doHoverRender(event.mouseX, event.mouseY, (GuiContainerAccessor) event.gui);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onKeyTyped(GuiScreenEvent.KeyboardInputEvent.Pre event) {
+        if (event.gui instanceof GuiContainer) {
+            SideMenu sidebar = SideMenuManager.getOrCreateSidebar((GuiContainer) event.gui);
+            int key = Keyboard.getEventKey();
+            char character = Keyboard.getEventCharacter();
+            if(!Keyboard.isKeyDown(key)) return;
+            // Draw sidebar hovering text
+            for (CustomElement element : sidebar.getElements().values()) {
+                if(element.isFocused()) {
+                    element.onKeyTyped(character,key);
+                }
+            }
+        }
     }
 
     public static void removeSidebar(GuiContainer gui) {
