54bd3bbd3ad05e599eb476f889f7f59b38eb52ab
MrFast <mrfastkrunker@gmail.com>
2024-01-20 13:23:23 -0500

    Added menu glass blending into chest

--- src/main/java/mrfast/sbf/features/items/HideGlass.java
+++ src/main/java/mrfast/sbf/features/items/HideGlass.java
@@ -10,19 +10,27 @@ import mrfast.sbf.utils.GuiUtils;
 import mrfast.sbf.utils.ItemUtils;
 import mrfast.sbf.utils.Utils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.IResource;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.ContainerChest;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
@@ -77,8 +85,9 @@ public class HideGlass {
     }
 
     public static boolean isEmptyGlassPane(ItemStack itemStack) {
-        return itemStack != null && (itemStack.getItem() == Item.getItemFromBlock(Blocks.stained_glass_pane)
-                || itemStack.getItem() == Item.getItemFromBlock(Blocks.glass_pane)) && itemStack.hasDisplayName() && Utils.cleanColor(itemStack.getDisplayName().trim()).isEmpty();
+        return itemStack != null &&
+                (itemStack.getItem() == Item.getItemFromBlock(Blocks.stained_glass_pane) || itemStack.getItem() == Item.getItemFromBlock(Blocks.glass_pane)) &&
+                itemStack.hasDisplayName() && itemStack.getMetadata() == 15 && Utils.cleanColor(itemStack.getDisplayName().trim()).isEmpty();
     }
 
     @SubscribeEvent
@@ -99,25 +108,58 @@ public class HideGlass {
         }
     }
 
+    public static int getPixelColor() {
+        try {
+            // Define the path to the chest texture
+            ResourceLocation chestTextureLocation = new ResourceLocation("minecraft", "textures/gui/container/generic_54.png");
+
+            // Load the texture as an image
+            IResource resource = Minecraft.getMinecraft().getResourceManager().getResource(chestTextureLocation);
+            BufferedImage chestImage = ImageIO.read(resource.getInputStream());
+
+            return chestImage.getRGB(5, 5);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return -1; // Return -1 if an error occurs
+    }
+
+    int pixelColor = -1;
+
+    @SubscribeEvent
+    public void onTextureStitchPre(TextureStitchEvent.Pre event) {
+        // Reload the pixel color when textures are stitched
+        pixelColor = getPixelColor();
+    }
+
     // Debug Mode to see all the slots ids
     @SubscribeEvent
-    public void onDrawSlots(GuiContainerEvent.DrawSlotEvent.Pre event) {
+    public void onDrawSlots(GuiContainerEvent.DrawSlotEvent.Post event) {
         if (event.gui instanceof GuiChest) {
             GuiChest gui = (GuiChest) event.gui;
             ContainerChest chest = (ContainerChest) gui.inventorySlots;
+            GlStateManager.translate(0f, 0f, 300f);
+            if (isEmptyGlassPane(event.slot.getStack()) && SkyblockFeatures.config.hideMenuGlass) {
+                GlStateManager.pushMatrix();
+                GL11.glColor4f(1f, 1f, 1f, 1f);
+                Gui.drawRect(
+                        event.slot.xDisplayPosition - 1,
+                        event.slot.yDisplayPosition - 1,
+                        event.slot.xDisplayPosition + 16 + 1,
+                        event.slot.yDisplayPosition + 16 + 1, pixelColor);
+                GlStateManager.popMatrix();
+            }
             if (Utils.isDeveloper()) {
                 if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) && Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
                     for (int i = 0; i < chest.inventorySlots.size(); i++) {
-
                         int x = chest.inventorySlots.get(i).xDisplayPosition;
                         int y = chest.inventorySlots.get(i).yDisplayPosition;
-                        GlStateManager.pushMatrix();
-                        GlStateManager.translate(0, 0, 700);
                         GuiUtils.drawText(ChatFormatting.GREEN + "" + i, x + 6, y + 6, GuiUtils.TextStyle.BLACK_OUTLINE);
-                        GlStateManager.popMatrix();
                     }
                 }
             }
+            GlStateManager.translate(0f, 0f, -300f);
         }
     }
 }
