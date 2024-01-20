f3e2bc633a9ac98e755367a1476baf83c2d7a3a5
MrFast <mrfastkrunker@gmail.com>
2024-01-20 13:22:20 -0500

    Add expand shortend number

--- src/main/java/mrfast/sbf/utils/Utils.java
+++ src/main/java/mrfast/sbf/utils/Utils.java
@@ -31,20 +31,20 @@ public class Utils {
 
     public static boolean inSkyblock = false;
     public static boolean inDungeons = false;
-    public static int health=0;
-    public static int maxHealth=0;
-    public static int mana=0;
-    public static int maxMana=0;
-    public static int overflowMana=0;
+    public static int health = 0;
+    public static int maxHealth = 0;
+    public static int mana = 0;
+    public static int maxMana = 0;
+    public static int overflowMana = 0;
 
     public static int Defense;
     public static final NumberFormat nf = NumberFormat.getInstance(Locale.US);
 
-    private static final String[] steps = new String[] {"", "k", "m", "b","t"};
+    private static final String[] steps = new String[]{"", "k", "m", "b", "t"};
 
     public static String shortenNumber(double number) {
-        if(number<1000) {
-            return ((int) number)+"";
+        if (number < 1000) {
+            return ((int) number) + "";
         }
         int magnitudeIndex = 0;
 
@@ -64,6 +64,26 @@ public class Utils {
         return formattedNumber + steps[magnitudeIndex];
     }
 
+    public static float expandShortenedNumber(String shortenedNumber) {
+        if(shortenedNumber.isEmpty()) return 0;
+        char lastChar = shortenedNumber.charAt(shortenedNumber.length() - 1);
+        float number = 0;
+        try{number = Float.parseFloat(shortenedNumber.substring(0, shortenedNumber.length() - 1));} catch (NumberFormatException ignored) {}
+
+        switch (lastChar) {
+            case 'k':
+                return number * 1000.0f;
+            case 'm':
+                return number * 1_000_000.0f;
+            case 'b':
+                return number * 1_000_000_000.0f;
+            case 't':
+                return number * 1_000_000_000_000.0f;
+            default:
+                return Float.parseFloat(shortenedNumber);
+        }
+    }
+
     /*
     Returns a int if it exists and 0 if it doesnt.
     Useful for /pv when certain APIs are turned off
@@ -75,6 +95,7 @@ public class Utils {
             return 0; // Default value when an exception occurs
         }
     }
+
     public static long safeGetLong(JsonObject jsonObject, String key) {
         try {
             return jsonObject.get(key).getAsLong();
@@ -83,27 +104,29 @@ public class Utils {
         }
     }
 
-	public static String percentOf(long num, long outOf) {
-		double percent = (double) num / outOf * 100;
-		return ChatFormatting.DARK_GRAY + " (" + String.format("%.2f", percent) + "%)";
-	}
-    
+    public static String percentOf(long num, long outOf) {
+        double percent = (double) num / outOf * 100;
+        return ChatFormatting.DARK_GRAY + " (" + String.format("%.2f", percent) + "%)";
+    }
+
     public static boolean isOnHypixel() {
         try {
             if (mc != null && mc.theWorld != null && !mc.isSingleplayer()) {
                 if (mc.thePlayer != null && mc.thePlayer.getClientBrand() != null) {
                     if (mc.thePlayer.getClientBrand().toLowerCase().contains("hypixel")) return true;
                 }
-                if (mc.getCurrentServerData() != null) return mc.getCurrentServerData().serverIP.toLowerCase().contains("hypixel");
+                if (mc.getCurrentServerData() != null)
+                    return mc.getCurrentServerData().serverIP.toLowerCase().contains("hypixel");
             }
             return false;
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
 
     static Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
     public static void copyToClipboard(String text) {
         clipboard.setContents(new StringSelection(text), null);
     }
@@ -119,10 +142,10 @@ public class Utils {
         double eyeY = mc.thePlayer.posY + mc.thePlayer.getEyeHeight();
         double eyeZ = mc.thePlayer.posZ;
         List<Vec3> corners = Arrays.asList(
-                new Vec3(targetBlockPos.getX()+0.1, targetBlockPos.getY() + 0.5, targetBlockPos.getZ()+0.1),
-                new Vec3(targetBlockPos.getX()+0.1, targetBlockPos.getY() + 0.5, targetBlockPos.getZ() + .9),
+                new Vec3(targetBlockPos.getX() + 0.1, targetBlockPos.getY() + 0.5, targetBlockPos.getZ() + 0.1),
+                new Vec3(targetBlockPos.getX() + 0.1, targetBlockPos.getY() + 0.5, targetBlockPos.getZ() + .9),
                 new Vec3(targetBlockPos.getX() + 0.9, targetBlockPos.getY() + 0.5, targetBlockPos.getZ() + .9),
-                new Vec3(targetBlockPos.getX() + 0.9, targetBlockPos.getY() + 0.5, targetBlockPos.getZ()+0.1)
+                new Vec3(targetBlockPos.getX() + 0.9, targetBlockPos.getY() + 0.5, targetBlockPos.getZ() + 0.1)
         );
         boolean visible = false;
         for (Vec3 corner : corners) {
@@ -130,7 +153,7 @@ public class Utils {
                     new Vec3(eyeX, eyeY, eyeZ),
                     corner
             );
-            if(checkRaytrace(rayTraceResult,targetBlockPos)) {
+            if (checkRaytrace(rayTraceResult, targetBlockPos)) {
                 visible = true;
                 break;
             }
@@ -145,14 +168,15 @@ public class Utils {
     /**
      * Taken from Danker's Skyblock Mod under GPL 3.0 license
      * https://github.com/bowser0000/SkyblockMod/blob/master/LICENSE
+     *
      * @author bowser0000
-    */
+     */
     public static void checkForSkyblock() {
         if (isOnHypixel()) {
-            if(mc.theWorld.getScoreboard() == null) return;
+            if (mc.theWorld.getScoreboard() == null) return;
             ScoreObjective scoreboardObj = mc.theWorld.getScoreboard().getObjectiveInDisplaySlot(1);
             if (scoreboardObj != null) {
-                String scObjName = ScoreboardUtil.fixFormatting(scoreboardObj.getDisplayName(),true);
+                String scObjName = ScoreboardUtil.fixFormatting(scoreboardObj.getDisplayName(), true);
                 if (scObjName.contains("SKYBLOCK")) {
                     inSkyblock = true;
                     return;
@@ -165,18 +189,19 @@ public class Utils {
     public static String cleanColor(String in) {
         return in.replaceAll("(?i)\\u00A7.", "");
     }
-    
+
     public static Minecraft GetMC() {
         return mc;
     }
 
     public static boolean overrideDevMode = false;
     public static boolean overrideDevModeValue = false;
+
     public static boolean isDeveloper() {
         return SkyblockFeatures.config.developerMode;
     }
 
-     public static String convertToTitleCase(String input) {
+    public static String convertToTitleCase(String input) {
         String[] words = input.split("_");
         StringBuilder result = new StringBuilder();
 
@@ -197,23 +222,23 @@ public class Utils {
         return result.toString();
     }
 
-	static int[] dungeonsXPPerLevel = {0, 50, 75, 110, 160, 230, 330, 470, 670, 950, 1340, 1890, 2665, 3760, 5260, 7380, 10300, 14400,
-									  20000, 27600, 38000, 52500, 71500, 97000, 132000, 180000, 243000, 328000, 445000, 600000, 800000,
-									  1065000, 1410000, 1900000, 2500000, 3300000, 4300000, 5600000, 7200000, 9200000, 12000000, 15000000,
-									  19000000, 24000000, 30000000, 38000000, 48000000, 60000000, 75000000, 93000000, 116250000};
+    static int[] dungeonsXPPerLevel = {0, 50, 75, 110, 160, 230, 330, 470, 670, 950, 1340, 1890, 2665, 3760, 5260, 7380, 10300, 14400,
+            20000, 27600, 38000, 52500, 71500, 97000, 132000, 180000, 243000, 328000, 445000, 600000, 800000,
+            1065000, 1410000, 1900000, 2500000, 3300000, 4300000, 5600000, 7200000, 9200000, 12000000, 15000000,
+            19000000, 24000000, 30000000, 38000000, 48000000, 60000000, 75000000, 93000000, 116250000};
 
     public static double xpToDungeonsLevel(double xp) {
-		for (int i = 0, xpAdded = 0; i < dungeonsXPPerLevel.length; i++) {
-			xpAdded += dungeonsXPPerLevel[i];
-			if (xp < xpAdded) {
-				double level =  (i - 1) + (xp - (xpAdded - dungeonsXPPerLevel[i])) / dungeonsXPPerLevel[i];
-				return (double) Math.round(level * 100) / 100;
-			}
-		}
-		return 50D;
-	}
-    
-    public static int randomNumber(int min,int max) {
+        for (int i = 0, xpAdded = 0; i < dungeonsXPPerLevel.length; i++) {
+            xpAdded += dungeonsXPPerLevel[i];
+            if (xp < xpAdded) {
+                double level = (i - 1) + (xp - (xpAdded - dungeonsXPPerLevel[i])) / dungeonsXPPerLevel[i];
+                return (double) Math.round(level * 100) / 100;
+            }
+        }
+        return 50D;
+    }
+
+    public static int randomNumber(int min, int max) {
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
     }
@@ -224,16 +249,17 @@ public class Utils {
         long min = (seconds / 60) % 60;
         long hours = (seconds / 3600) % 24;
         long days = seconds / (3600 * 24);
-    
+
         if (days > 0) time += days + "d ";
         if (hours > 0) time += hours + "h ";
         if (min > 0) time += min + "m ";
         time += sec + "s";
-    
+
         return time.trim();
     }
-    public static String msToSeconds(long ms,int precision) {
-        return Utils.round(ms/1000d,precision)+"s";
+
+    public static String msToSeconds(long ms, int precision) {
+        return Utils.round(ms / 1000d, precision) + "s";
     }
 
     // Precision is # of decimal places
@@ -247,14 +273,15 @@ public class Utils {
         final Collection<NetworkPlayerInfo> players = Utils.GetMC().getNetHandler().getPlayerInfoMap();
         final List<String> list = new ArrayList<>();
         for (final NetworkPlayerInfo info : players) {
-            if(!info.getGameProfile().getName().contains("!")) list.add(info.getGameProfile().getName());
+            if (!info.getGameProfile().getName().contains("!")) list.add(info.getGameProfile().getName());
         }
         return list.toArray(new String[0]);
     }
-    
+
     /**
      * Taken from Danker's Skyblock Mod under GPL 3.0 license
      * https://github.com/bowser0000/SkyblockMod/blob/master/LICENSE
+     *
      * @author bowser0000
      */
     public static void checkForDungeons() {
@@ -273,13 +300,13 @@ public class Utils {
 
     public static void sendMessage(String string) {
         if (Utils.GetMC().ingameGUI != null && Utils.GetMC().thePlayer != null) {
-            Utils.GetMC().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(ChatFormatting.AQUA+"[SBF] "+ChatFormatting.RESET+string));
+            Utils.GetMC().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(ChatFormatting.AQUA + "[SBF] " + ChatFormatting.RESET + string));
         }
     }
 
     public static void sendMessage(IChatComponent msg) {
         if (Utils.GetMC().ingameGUI != null || Utils.GetMC().thePlayer == null) {
-            ChatComponentText prefix = new ChatComponentText(EnumChatFormatting.AQUA+"[SBF] "+EnumChatFormatting.RESET);
+            ChatComponentText prefix = new ChatComponentText(EnumChatFormatting.AQUA + "[SBF] " + EnumChatFormatting.RESET);
             Utils.GetMC().thePlayer.addChatMessage(new ChatComponentText("").appendSibling(prefix).appendSibling(msg));
         }
     }
@@ -289,20 +316,21 @@ public class Utils {
     }
 
     public static boolean isNPC(Entity entity) {
-        if(entity instanceof EntityPlayer) {
+        if (entity instanceof EntityPlayer) {
             EntityLivingBase entityLivingBase = (EntityLivingBase) entity;
             return entity.getUniqueID().version() == 2 && entityLivingBase.getHealth() == 20.0F && !entityLivingBase.isPlayerSleeping() && Utils.inSkyblock;
         } else return false;
     }
 
     public static void setTimeout(Runnable code, int ms) {
-        setTimeout(code,ms,false);
+        setTimeout(code, ms, false);
     }
-    public static void setTimeout(Runnable code, int ms,boolean addToMinecraft) {
+
+    public static void setTimeout(Runnable code, int ms, boolean addToMinecraft) {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                if(addToMinecraft) Utils.GetMC().addScheduledTask(code);
+                if (addToMinecraft) Utils.GetMC().addScheduledTask(code);
                 else code.run();
             }
         }, ms);
@@ -327,7 +355,7 @@ public class Utils {
             case "farming_1":
                 return "The Farming Islands";
             case "foraging_1":
-                return "The Park";    
+                return "The Park";
             default:
                 return Utils.convertToTitleCase(id);
         }
